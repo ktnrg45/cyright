e9b3bb600052fc51133e34f1bf378c23b571faf9
ktnrg45 <ktnrg45dev@gmail.com>
2022-10-07 01:39:40 -0700

    Add more cytho builtins

--- packages/pyright-internal/typeshed-fallback/stdlib/cython_builtins.pyi
+++ packages/pyright-internal/typeshed-fallback/stdlib/cython_builtins.pyi
@@ -10,54 +10,92 @@ _I = TypeVar("_I", bound=bool|int|float)
 
 class NULL(_N):
     """Cython Type. Represents null."""
+    ...
 
 
 class void(_N):
     """Cython Type. Represents void."""
+    ...
 
 
 class bint(_B):
     """Cython Type. Represents a boolean integer."""
+    ...
 
 
 class char(_B):
     """Cython Type. Represents a char integer."""
+    ...
 
 
 class short(_B):
     """Cython Type. Represents a short integer."""
+    ...
 
 
 class size_t(_B):
     """Cython Type. Represents size_t."""
+    ...
 
 
 class ssize_t(_B):
     """Cython Type. Represents ssize_t."""
+    ...
 
 
 class long(_I):
     """Cython Type. Represents a long integer."""
+    ...
 
 
 class complex(_I):
     """Cython Type. Represents a complex integer."""
+    ...
 
 
 class double(_I):
     """Cython Type. Represents a double."""
+    ...
 
 
 class struct(_T):
     """Cython Type. Represents a struct."""
+    ...
 
 
 class enum(_T):
     """Cython Type. Represents a enum."""
+    ...
 
 
 class union(_T):
     """Cython Type. Represents a union."""
+    ...
+
+
+class Py_UNICODE(_T):
+    """Cython Type. Represents a Py_UNICODE. Equivalent to wchar_t."""
+    ...
+
+
+class Py_UCS4(_T):
+    """Cython Type. Represents a Py_UCS4. Single unicode character."""
+    ...
+
+
+class Py_hash_t(_T):
+    """Cython Type. Represents a Py_hash_t. Hash of object. Size of Py_ssize_t."""
+    ...
+
+
+class Py_ssize_t(_T):
+    """Cython Type. Represents a Py_ssize_t."""
+    ...
+
+
+class Py_tss_t(_T):
+    """Cython Type. Represents a Py_tss_t. Thread state."""
+    ...
 
 
 def sizeof(__obj: Any) -> int:
