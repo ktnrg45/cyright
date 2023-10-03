b7857a6a01748a0a17e7d74a73948820e1cd3e10
Joe Renaud <joe.p.renaud@gmail.com>
2023-10-02 20:36:35 -0400

    Added new `noexcept gil` error

--- packages/pyright-internal/src/localization/localize.ts
+++ packages/pyright-internal/src/localization/localize.ts
@@ -1004,6 +1004,7 @@ export namespace Localizer {
         export const expectedFileName = () => getRawString('Diagnostic.expectedFileName');
         export const expectedWith = () => getRawString('Diagnostic.expectedWith');
         export const expectedNoGil = () => getRawString('Diagnostic.expectedNoGil');
+        export const noexceptWithGil = () => getRawString('Diagnostic.noexceptWithGil');
         export const expectedCastClose = () => getRawString('Diagnostic.expectedCastClose');
         export const deprecatedPropertyCython = () => getRawString('Diagnostic.deprecatedPropertyCython');
         export const deprecatedForFromLoop = () => getRawString('Diagnostic.deprecatedForFromLoop');
